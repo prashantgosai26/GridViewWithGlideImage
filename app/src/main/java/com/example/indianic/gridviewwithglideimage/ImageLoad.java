1ca2d4517336d1f2684e548378653625d7603202
prashant <prashantgosai26@gmail.com>
2016-04-05 12:24:57 +0530

    new on twith full database

--- app/src/main/java/com/example/indianic/gridviewwithglideimage/ImageLoad.java
+++ app/src/main/java/com/example/indianic/gridviewwithglideimage/ImageLoad.java
@@ -1,7 +1,10 @@
 package com.example.indianic.gridviewwithglideimage;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,12 +13,25 @@ public class ImageLoad extends AppCompatActivity {
 
     private String strURL;
     private ImageView imageView;
+    private ImageView ivFullScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_load);
         imageView = (ImageView) findViewById(R.id.imageoad_iv);
+        ivFullScreen = (ImageView) findViewById(R.id.imageoad_iv_full);
         strURL = getIntent().getStringExtra("url");
         Glide.with(this).load(strURL).into(imageView);
+        ivFullScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 }
